cdddd43a715da9184feebf3dd9b0cc96e85909f7
Bin Ury <1146921+teddy-error@users.noreply.github.com>
2018-11-06 00:53:19 -0600

    Add dotenv loading

--- src/octane.js
+++ src/octane.js
@@ -1,16 +1,35 @@
 const path = require('path');
 const util = require('util');
+const fs = require('fs');
 
 const _ = require('lodash');
 const program = require('commander');
 const inquire = require('inquirer');
 const chalk = require('chalk');
+const dotenv = require('dotenv');
+
+const config = { parsed: {}};
+loadUserConfig();
 
 program
   .version(require('../package.json').version)
-  // .usage('')
   .action(() => {
     console.log('Hello, world!');
   });
+
+program
+  .command('config')
+  .action(() => {
+    console.log(config);
+  });
+
+
+function getUserConfig(configPath = path.resolve(process.env.HOME, '.env')) {
+  return fs.existsSync(configPath) && dotenv.config({ path: configPath });
+}
+
+function loadUserConfig() {
+  _.assign(config, getUserConfig());
+}
 
 program.parse(process.argv);
