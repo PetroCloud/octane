f74a84e3acad34e9bb5f2dd5c4d4ab17c4ac358b
Bin Ury <1146921+teddy-error@users.noreply.github.com>
2018-11-06 01:49:46 -0600

    Add writeConfig util function
    - Refactor config path as constant

--- src/util.js
+++ src/util.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
+const _ = require('lodash');
+
+const CONFIG_PATH = path.resolve(process.env.HOME, '.octane');
 
 const util = {
-  getUserConfig(configPath = path.resolve(process.env.HOME, '.octane')) {
+  getUserConfig(configPath = CONFIG_PATH) {
     return fs.existsSync(configPath) && dotenv.config({ path: configPath });
+  },
+
+  writeConfig(config = {}) {
+    const parsedConfig = _.map(config, (val, key) => `${key}=${val}`).join('\n');
+    return fs.writeFileSync(CONFIG_PATH, parsedConfig)
   }
 }
 
