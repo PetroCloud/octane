52cb4c82cf162df235b5f765898fd25de47051cb
Bin Ury <1146921+teddy-error@users.noreply.github.com>
2018-11-06 02:46:34 -0600

    Add writeConfig test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 import test from 'ava';
+
+const fs = require('fs');
 const mock = require('mock-fs');
 const path = require('path');
 
 const util = require('../src/util');
+
 const FAKE_ROOT = '/Users/octane/home';
 const FAKE_FILE = '.test';
 
@@ -17,6 +20,17 @@ test.before(t => {
 test('getUserConfig', t => {
 	const config = util.getUserConfig(path.join(FAKE_ROOT, FAKE_FILE));
 	t.deepEqual(config.parsed, { TEST: 'whoopie' });
+});
+
+test('writeConfig', t => {
+	const mock = { WRITE: 'rite' };
+	const writePath = path.join(FAKE_ROOT, '.write')
+
+	t.false(fs.existsSync(writePath));
+	util.writeConfig(mock, writePath);
+
+	const config = util.getUserConfig(writePath);
+	t.deepEqual(config.parsed, mock);
 });
 
 test.after.always('Teardown', t => {
