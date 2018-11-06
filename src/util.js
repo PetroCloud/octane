74e3dfde3baa151705fc55247dc0875f07e1f6b6
Bin Ury <1146921+teddy-error@users.noreply.github.com>
2018-11-06 01:11:02 -0600

    Rename default configuration file path to .octane

--- src/util.js
+++ src/util.js
@@ -3,7 +3,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 
 const util = {
-  getUserConfig(configPath = path.resolve(process.env.HOME, '.env')) {
+  getUserConfig(configPath = path.resolve(process.env.HOME, '.octane')) {
     return fs.existsSync(configPath) && dotenv.config({ path: configPath });
   }
 }
