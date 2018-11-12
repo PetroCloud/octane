c2dc306e44bced7be19922cbed10fdfb394ffe99
Bin Ury <1146921+teddy-error@users.noreply.github.com>
2018-11-12 17:50:57 -0600

    Add fallback config generation for cases where none yet exists

--- src/util.js
+++ src/util.js
@@ -3,17 +3,26 @@ const path = require('path');
 const dotenv = require('dotenv');
 const _ = require('lodash');
 
-const CONFIG_PATH = path.resolve(process.env.HOME, '.octane');
+const CONFIG_ROOT_PATH = path.join(process.env.HOME, '.config', 'octane');
+const CONFIG_PATH = path.join(CONFIG_ROOT_PATH, '.env');
+
+// Ensure config exists
+!fs.existsSync(CONFIG_ROOT_PATH) && fs.mkdirSync(CONFIG_ROOT_PATH, { recursive: true });
+// Write empty config
+!fs.existsSync(CONFIG_PATH) && writeConfig();
+
+function getUserConfig(configPath = CONFIG_PATH) {
+  return fs.existsSync(configPath) && dotenv.config({ path: configPath });
+}
+
+function writeConfig(config = {}, configPath = CONFIG_PATH) {
+  const parsedConfig = _.map(config, (val, key) => `${key}=${val}`).join('\n');
+  return fs.writeFileSync(configPath, parsedConfig)
+}
 
 const util = {
-  getUserConfig(configPath = CONFIG_PATH) {
-    return fs.existsSync(configPath) && dotenv.config({ path: configPath });
-  },
-
-  writeConfig(config = {}, configPath = CONFIG_PATH) {
-    const parsedConfig = _.map(config, (val, key) => `${key}=${val}`).join('\n');
-    return fs.writeFileSync(configPath, parsedConfig)
-  }
+  getUserConfig,
+  writeConfig
 }
 
 module.exports = util;
