0134ba96b9fa3eb59dc93b93d8e756222edbd0f5
Bin Ury <1146921+teddy-error@users.noreply.github.com>
2018-11-12 18:42:14 -0600

    Add start command
    - Add execa
    - Add example env file
    - Refactor default config object assignment

--- src/octane.js
+++ src/octane.js
@@ -2,24 +2,40 @@
 // const chalk = require('chalk');
 const _ = require('lodash');
 const program = require('commander');
+const execa = require('execa');
 const util = require('./util');
 
-const config = { parsed: {}};
-const userConfig = util.getUserConfig();
-_.assign(config, userConfig);
+const _default = { parsed: {}};
+const config = { ..._default.parsed, ...util.getUserConfig().parsed };
 
 program
   .version(require('../package.json').version)
   .action(() => {
-    console.log('Hello, world!');
+    console.log('Default action');
   });
 
 program
   .command('config')
   .action(() => {
-    return userConfig ?
-      console.log(config) :
-      console.warn('No configuration file exists, would you like to create one?');
+    return _.isEmpty(config) ?
+      console.warn('Oops! No config exists yet. Check docs for usage instructions.') :
+      console.dir(config);
   });
+
+program
+  .command('start')
+  .action(() => run('docker-compose up -d'));
+
+/**
+ * Run - execa wrapper
+ * @param {String} command
+ * @param {Object} [options]
+ * @param {Object} [options.env] Key-value pairs to pass to the child process env
+ * @param {String} [options.cwd] Current working directory of the child process
+ * @returns {Promise}
+ */
+function run(command, { env = config, cwd = config.PROJECT_ROOT } = {}) {
+  return execa.shell(command, { env, cwd, stdio: 'inherit'});
+}
 
 program.parse(process.argv);
